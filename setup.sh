0631d01904b6f855ceb966ff8eb00b41a5bd6faa
coyotekojote <koyote025@gmail.com>
2022-07-24 20:30:49 +0900

    add setup script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/starship.toml ~/.config/starship.toml
+ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/fish/ ~/.config/
