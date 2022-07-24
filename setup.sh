8d6ddee33fbb5b362a0c9249a58768cb92f4a32e
coyotekojote <koyote025@gmail.com>
2022-07-24 21:27:20 +0900

    setupファイルにfish関連のスクリプト追加

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+
+# シンボリックリンクを貼る
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/starship.toml ~/.config/starship.toml
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/fish/ ~/.config/
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/brew/Brewfile ~/.Brewfile
+
+# brewのパッケージインストール
+brew bundle --global
+
+# fish設定
+echo $(which fish) | sudo tee -a /etc/shells
+chsh -s $(which fish)
+
+# fisherインストール
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
