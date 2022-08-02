e3bdafdc14124867a9209280325d42ce94d3a1a0
coyotekojote <koyote025@gmail.com>
2022-08-02 13:56:32 +0900

    ctr w でpeco_select_historyが動くように

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,10 @@ status --is-interactive; and rbenv init - fish | source
 starship init fish | source
 
 zoxide init fish | source
+
+#peco setting
+set fish_plugins theme peco
+
+function fish_user_key_bindings
+  bind \cw peco_select_history
+end
