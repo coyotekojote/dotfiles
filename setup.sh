cc7bd3e55527fb912ca001320af36d0d61a50dd9
coyotekojote <koyote025@gmail.com>
2025-01-04 08:55:02 +0900

    Update setup.sh
    zsh関連のシンボリックリンク追加

--- setup.sh
+++ setup.sh
@@ -2,8 +2,9 @@
 
 brew install --cask ghostty
 
-
 mkdir -p ~/etc/ && echo "ZDOTDIR=$HOME/.config/zsh" > ~/etc/zshenv
 
-
+ln -s ~/ghq/github.com/coyotekojote/dotfiles/zsh ~/.config/
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/ghostty ~/.config/
+
+
