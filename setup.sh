ed33873eb48bb1a9135ce501a97b8e3dd35076fd
coyotekojote <koyote025@gmail.com>
2025-01-03 21:54:14 +0900

    Update setup.sh
    まずはghosttyのinstallだけにした

--- setup.sh
+++ setup.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
 
+brew install --cask ghostty
 
-# シンボリックリンクを貼る
-ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/starship.toml ~/.config/starship.toml
-ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/fish/ ~/.config/
-ln -s ~/ghq/github.com/coyotekojote/dotfiles/config/brew/Brewfile ~/.Brewfile
-
-# brewのパッケージインストール
-brew bundle --global
-
-# fish設定
-echo $(which fish) | sudo tee -a /etc/shells
-chsh -s $(which fish)
-
-# fisherインストール
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
