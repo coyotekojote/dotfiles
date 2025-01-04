d23c219002c36d1568aff3b446606e8d3c5b1124
coyotekojote <koyote025@gmail.com>
2025-01-04 09:32:56 +0900

    ZDOTDIRの設定スクリプト修正

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 brew install --cask ghostty
 
-mkdir -p ~/etc/ && echo "ZDOTDIR=$HOME/.config/zsh" > ~/etc/zshenv
+sudo echo "ZDOTDIR=$HOME/.config/zsh" > /etc/zshenv
 
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/zsh ~/.config/
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/ghostty ~/.config/
