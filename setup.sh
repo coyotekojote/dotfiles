f484e6aa5c8146e991a238f019d9bfd0ac0b713f
coyotekojote <yohei_otake@smartbank.co.jp>
2025-04-25 14:18:19 +0900

    set_up.sh修正

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 brew install --cask ghostty
 
-sudo echo "ZDOTDIR=$HOME/.config/zsh" > /etc/zshenv
+echo "ZDOTDIR=$HOME/.config/zsh" | sudo tee /etc/zshenv > /dev/null
 
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/zsh ~/.config/
 ln -s ~/ghq/github.com/coyotekojote/dotfiles/ghostty ~/.config/
